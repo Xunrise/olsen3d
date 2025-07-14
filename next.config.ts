c16de3f195e21b4070b1c48b3c49045a93eb3de5
Martin Galusinski <16625871+Xunrise@users.noreply.github.com>
2025-07-14 13:05:00 +0200

    Remove static output generation and workaround for that

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  //Custom loader for building static webpage and fixing image links for next/image component
-  images: {
-    loader: 'custom',
-    loaderFile: './src/utils/imageLoader.ts'
-  }
+ 
 };
 
 export default nextConfig;
