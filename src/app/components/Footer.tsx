b357850e4f070c8999d007ff6f4eb8187a6a36de
Martin Galusinski <16625871+Xunrise@users.noreply.github.com>
2025-10-09 13:31:15 +0000

    Break in adress in footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
         <div className={styles.contactInfo}>
           <p>E-post: <a href="mailto:post@olsen3d.no">post@olsen3d.no</a></p>
           <p>Telefon: <a href="tel:+4748227272">+47 482 27 272</a></p>
-          <p>Adresse: Olderdalen, 9146</p>
+          <p>Adresse: <br></br> Olderdalen, 9146</p>
         </div>
         <p>&copy; 2025 Olsen3D. Alle rettigheter reservert.</p>
       </div>
